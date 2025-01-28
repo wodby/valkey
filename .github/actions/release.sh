919a4a5443b46f74e712aa305f98beea856af8db
Chingis Sandanov <chingis@chingis.io>
2025-01-28 13:24:25 +0300

    Test parallel multi-arch build

--- .github/actions/release.sh
+++ .github/actions/release.sh
@@ -3,8 +3,6 @@
 set -e
 
 if [[ "${GITHUB_REF}" == refs/heads/main || "${GITHUB_REF}" == refs/tags/* ]]; then
-    docker login -u "${DOCKER_USERNAME}" -p "${DOCKER_PASSWORD}"
-
     if [[ "${GITHUB_REF}" == refs/tags/* ]]; then
       export STABILITY_TAG="${GITHUB_REF##*/}"
     fi
